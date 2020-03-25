9a021ce71121e2d9d84e5fad73d87774ed1238c2
Bogdan261 <0bogdan261@gmail.com>
2020-03-25 17:31:04 +0200

    File upload

---
+++ Laborator4/Class1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Laborator4
+{
+    public class Class1
+    {
+    }
+
+
+    public partial class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Stock { get; set; }
+        public double Price { get; set; }
+        public int Deleted { get; set; }
+        public ICollection<OrderDetails> OrderDetails {get; set;}
+        public int OrderDetailsID { get; set; }
+
+
+
+
+    }
+    public partial class OrderDetails
+    {
+        public int OrderDetailsId  {get; set;}
+        public int ProductId { get; set; }
+        public double Qty { get; set; }
+        public double Price { get; set; }
+        public int Deleted { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+
+        protected OrderDetails() { }
+
+
+
+
+    }
+
+    public partial class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime Date { get; set; }
+        public double Value { get; set; }
+        public int Payed { get; set; }
+        public int ClientId { get; set; }
+        public int Deleted { get; set; }
+        public Client Client { get; set; }
+        public ICollection<OrderDetails> OrderDetails { get; set; }
+
+        protected Order() { }
+
+
+
+    }
+    public partial class Client 
+    {
+    public int ClientId { get; set; }
+    
+    public string Name { get; set; }
+   
+    public string Email { get; set; }
+    // 0 = inregistrare corecta; 1 =inregistrare stearsa//
+    public int Deleted { get; set; }
+    public ICollection<Order> Orders { get; set; }
+     protected Client() { } 
+    
+    
+    }
+
+
+
+    internal class ModelContext: DbContext
+    {
+        public DbSet <Client> Clients { get; set; }
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=DESKTOP-BHHLHIS\\SQLEXPRESS;Database=EFCore2020;Trusted_Connection=True");//ChangeTracker.LazyLoadingEnabled = false;
+        
+        }
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>()
+                .HasMany<Order>(o => o.Orders)
+                .WithOne(c => c.Client);
+            modelBuilder.Entity<OrderDetails>()
+                .HasOne<Order>(od => od.Order)
+                .WithMany(o => o.OrderDetails); 
+            modelBuilder.Entity<Product>()
+                .HasMany<OrderDetails>(p => p.OrderDetails)
+                .WithOne(od => od.Product);
+
+
+
+        }
+
+
+
+    }
+
+}
---
+++ Laborator4/Migrations/20200311182511_Tabelele.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Laborator4.Migrations
+{
+    public partial class Tabelele : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Value = table.Column<double>(nullable: false),
+                    Payed = table.Column<int>(nullable: false),
+                    ClientId = table.Column<int>(nullable: false),
+                    Deleted = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_Order_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "ClientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: true),
+                    Stock = table.Column<double>(nullable: false),
+                    Price = table.Column<double>(nullable: false),
+                    Deleted = table.Column<int>(nullable: false),
+                    OrderDetailsID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderDetailsId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(nullable: false),
+                    Qty = table.Column<double>(nullable: false),
+                    Price = table.Column<double>(nullable: false),
+                    Deleted = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderDetailsId);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Order_ClientId",
+                table: "Order",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetails",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ProductId",
+                table: "OrderDetails",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Order");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
